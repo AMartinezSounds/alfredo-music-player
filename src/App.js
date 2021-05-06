3a4bf66196f4797d2486cc137ca29ef802a6abb6
Alfredo Martínez <alfre_canos13@icloud.com>
2021-05-06 17:18:44 +0200

    I've imported util.js to App.js to have the data available. Then I've used two useState hooks to track all the songs and the current song. Then I've passed down the state of the current song to Song.js and I've replaced the hardcoded data with the current song data

--- src/App.js
+++ src/App.js
@@ -1,14 +1,20 @@
+import React, { useState } from 'react';
 
 import './App.css';
 import Song from './components/Song';
 import Player from './components/Player';
 
+import data from './util';
+
 import "./styles/app.scss";
 
 function App() {
+  // State
+  const [songs, setSongs] = useState(data());
+  const [currentSong, setCurrentSong] = useState(songs[0]);
   return (
     <div className="App">
-      <Song/>
+      <Song currentSong={currentSong}/>
       <Player/>
     </div>
   );
