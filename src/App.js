31a9232c3fc80fbcc6516d5489f55c9046d3666d
Alfredo Martínez <alfre_canos13@icloud.com>
2021-05-06 10:45:55 +0200

    I've created the components Song.js and Player.js, I've added some hardcoded data and then I've installed the Font Awesome library to add the play, skip-back and skip-forward buttons

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 
 import './App.css';
+import Song from './components/Song';
+import Player from './components/Player';
 
 function App() {
   return (
     <div className="App">
-      
+      <Player/>
+      <Song/>
     </div>
   );
 }
---
+++ src/components/Song.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Song = () => {
+    return (
+        <div className='song-container'>
+            <h1>Picture</h1>
+            <h2>Song Name</h2>
+            <h1>Artist</h1>
+        </div>
+    );
+}
+
+export default Song;
