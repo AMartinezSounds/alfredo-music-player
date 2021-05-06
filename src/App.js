f815e46fce148bd1675548e7a149013121791aaf
Alfredo Martínez <alfre_canos13@icloud.com>
2021-05-06 18:34:02 +0200

    I've used useRef ito be able to select the audio HTML tag in my player component. Then I set another state in App.js to keep track on the song playing (it starts false). Lastly, in Player.js I set a function that checks if isPlaying is true or false to play or pause the song

--- src/App.js
+++ src/App.js
@@ -11,11 +11,12 @@ import "./styles/app.scss";
 function App() {
   // State
   const [songs, setSongs] = useState(data());
-  const [currentSong, setCurrentSong] = useState(songs[1]);
+  const [currentSong, setCurrentSong] = useState(songs[0]);
+  const [isPlaying, setIsPlaying] = useState(false);
   return (
     <div className="App">
       <Song currentSong={currentSong}/>
-      <Player/>
+      <Player isPlaying={isPlaying} setIsPlaying={setIsPlaying} currentSong={currentSong}/>
     </div>
   );
 }
