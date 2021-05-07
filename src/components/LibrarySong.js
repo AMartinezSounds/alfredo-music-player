85408902efd27a441429a615521a1cae25fa9143
Alfredo Martínez <alfre_canos13@icloud.com>
2021-05-07 09:27:33 +0200

    I've created the partial _library.scss to style the library.. I've styled the library and added the font Lato

--- src/components/LibrarySong.js
+++ src/components/LibrarySong.js
@@ -4,8 +4,10 @@ const LibrarySong = ({ song }) => {
     return (
         <div className='library-song'>
             <img alt="cover" src={song.cover}></img>
-            <h3>{song.name}</h3>
-            <h4>{song.artist}</h4>
+            <div className="song-description">
+                <h3>{song.name}</h3>
+                <h4>{song.artist}</h4>
+            </div>
         </div>
     );
 }
